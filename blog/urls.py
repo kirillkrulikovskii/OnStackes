dd22175bd11d62e2d46e34cb48239dba98aa8c32
KKrulikovskii09 <115092022+kirillkrulikovskii@users.noreply.github.com>
2026-03-08 13:39:25 +0000

    edited blog urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('fetch_comments/<int:pk>/', views.fetch_comments, name='fetch_comments'),
-    path('post_list/'      , views.PostListView.as_view()  , name='post_list'  ),
-    path('<int:pk>/'       , views.PostDetailView.as_view(), name='post_detail'),
+    # path('post_list/'      , views.PostListView.as_view()  , name='post_list'  ),
+    # path('<int:pk>/'       , views.PostDetailView.as_view(), name='post_detail'),
     path('create/'         , views.create_post             , name='create_post'),
     path('<int:pk>/edit/'  , views.edit_post               , name='edit_post'  ),
     path('<int:pk>/delete/', views.delete_post             , name='delete_post'),
